debc1b2b1c949b0382abee855624c945b719b3c5
Narmada Gogineni <93468483+narmadagogineni@users.noreply.github.com>
2023-01-30 20:54:34 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,13 @@
+tup=(2,4,5,7,5,98)              #duplicates allowed 
+print(tup)
+
+print(tup[1])
+
+#tup[2]=43  #error bcz tuplr doesnt support item assignment
+#tup(1,3)   #error bcz tuplr doesnt support item assignment
+
+tupp=(35)  #wrong way, it just prints num without ()
+tup1=(35,)  #tuple with one elemnt reuires , at end
+print(tup1)
+tup2=()   #empty tuple is posssible
+print(tup2)
